979c659c6ca96182c6a706a16fb23b587b5c63bf
Syrena12 <1029982427@qq.com>
2023-06-12 22:26:33 +0800

    v2

--- app.py
+++ app.py
@@ -7,153 +7,122 @@ from flask import (Flask, redirect, render_template, request,
 
 app = Flask(__name__)
 
+df = pd.read_csv('resources/people.csv')
+# ,header=0,index_col=0)
+
+
+def is_number(s):
+    try:  # 如果能运行float(s)语句，返回True（字符串s是浮点数）
+        float(s)
+        return True
+    except ValueError:  # ValueError为Python的一种标准异常，表示"传入无效的参数"
+        pass  # 如果引发了ValueError这种异常，不做任何事情（pass：不做任何事情，一般用做占位语句）
+    try:
+        import unicodedata  # 处理ASCii码的包
+        unicodedata.numeric(s)  # 把一个表示数字的字符串转换为浮点数返回的函数
+        return True
+    except (TypeError, ValueError):
+        pass
+    return False
+
 @app.route('/')
 def index():
-    img_url = url_for('static', filename='images/cat.jpg')
+    img_url = url_for('static', filename='images/dar.jpg')
+    print(df)
     return render_template('index3.html',img_url=img_url)
 
-data = []
-with open('resources/q0c.csv', mode='r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    for row in csv_reader:
-        data.append(row)
-
-@app.route('/search_by_room', methods=['POST'])
-def search_by_room():
-    img_path = ''
-    room = request.form['room']
+@app.route('/search_by_name', methods=['POST'])
+def search_by_name():
     result = []
-    for row in data:
-        if row['room'] == room:
-            result.append(row)
-            img_path = row['pic']
-            break
-    if img_path:
-        img_url = url_for('static', filename='images/'+img_path)
+    name = request.form['Name']
+    find=df[df['Name'] == name]
+    if find.shape[0]>0:
+        find['img_url']= '/static/images/'+find['Picture']
+        result=find.to_dict(orient='records')
+        # print("===========================")
+        # print(type(result))
+        # print(result)
+    return render_template('result.html', result=result)
+
+@app.route('/search_by_salary', methods=['POST'])
+def search_by_salary():
+    result = []
+    start_salary = 0
+    if is_number(request.form['End_salary']) :
+        end_salary = float(request.form['End_salary'])
+        if is_number(request.form['Start_salary']):
+            start_salary = float(request.form['Start_salary'])
+        for index,row in df.iterrows():
+            print ('row Salary')
+            print (type(row['Salary']))
+            if is_number(row['Salary']):
+                salary = float(row['Salary'])
+                if start_salary <= salary <= end_salary:
+                    row['img_url']= '/static/images/'+row['Picture']
+                    result.append(row)
+        return render_template('result.html', result=result)
     else:
-        img_url = 'no information or picture available'
+        return 'Input Error'    
 
-    return render_template('search_by_room.html', result=result,img_url=img_url)
-
-@app.route('/search_by_teln', methods=['POST'])   #好像是坏的
-def search_by_teln():
-    start_teln = 0
-    if request.form['start_teln'].isdigit():
-        start_teln = int(request.form['start_teln'])
-    end_teln = int(request.form['end_teln'])
-    result = []
-    img_path=''
-    for row in data:
-        if row['teln'].isdigit():
-            teln = int(row['teln'])
-            if start_teln <= teln <= end_teln:
-                img_path = row['pic']
-                if img_path:
-                    row['pic'] = url_for('static', filename='images/'+img_path)
-                else:
-                    row['pic'] = 'no information or picture available'
-                result.append(row)
-    if len(result)==0:
-        row={'name':'no information or picture available'}
-        result.append(row)
-    return render_template('search_by_teln.html', result=result)
-
-# @app.route('/update_description', methods=['POST'])
-# def update_description():
-#     teln = request.form['teln']
-    
-#     new_description = request.form['new_description']
-#     message='Teln number not found.'
-#     for row in data:
-#         if row['Telnum'] == teln:
-#             row['descript'] = new_description
-#             # 怎么save到csv里
-            
-#             # # path为输出路径和文件名，newline=''是为了不出现空行
-#             # csvFile = open('resources/temp.csv', "w+",newline='')
-#             # try:
-#             #     # data为list类型
-#             #     for i in range(len(data)):
-#             #         writer.writerow(data[i])
-#             # finally:
-#             #     csvFile.close()
+@app.route('/upload_zip', methods=['POST'])
+def upload_zip():
+    print('==================================')
+    f = request.files['zipFile']
+    print(type(f))
+    pass
 
 
-#             ##################
-#             message= 'Description updated successfully!'
-#             break
-#     return render_template('update_description.html', message=message)
-    
+@app.route('/update_user', methods=['POST'])
+def update_user():
+    global df
+    name = request.form['Name']
+    state = request.form['State']
+    salary = request.form['Salary']
+    grade = request.form['Grade']
+    room = request.form['Room']
+    telnum = request.form['Telnum']
+    keywords = request.form['Keywords']
+    # picture = request.form['Picture']
+
+    find=df[df['Name'] == name]
+    if find.shape[0]>0:
+        if state!='' :
+            df.loc[df['Name']==name,'State']=state
+        if salary!='':
+            if is_number(salary):
+                df.loc[df['Name']==name,'Salary']=salary
+            else:
+                return 'Update Salary Failed'
+        if grade!='':
+            if is_number(grade):
+                df.loc[df['Name']==name,'Grade']=grade
+            else:
+                return 'Update Grade Failed'
+        if room!='':
+            if is_number(room):
+                df.loc[df['Name']==name,'Room']=room
+            else:
+                return 'Update Room Failed'
+        if telnum!='' :
+            if is_number(telnum):
+                df.loc[df['Name']==name,'Telnum']=telnum
+            else:
+                return 'Update Telnum Failed'
+        if keywords!='':
+            df.loc[df['Name']==name,'Keywords']=keywords
+        return 'Update Success' 
+    return 'Found No User'
 
 
-
-
-#================================================================================================================================
-
-
-# filename = 'resources/q0c.csv'  # 修改为 csv 文件路径
-# img_dir = 'resources/'  # 修改为图片路径
-
-
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     img_path = ''
-#     if request.method == 'POST':
-#         search = request.form.get('search')
-#         with open(filename, newline='') as f:
-#             reader = csv.reader(f)
-#             next(reader)
-#             for row in reader:
-#                 if search == row[0]:
-#                     img_path = row[2]
-#                     break
-#         if img_path:
-#             img_url = url_for('img_file', filename=img_path)
-#         else:
-#             img_url = url_for('static', filename='images/cat.jpg')
-#     else:
-#         img_url = url_for('static', filename='images/cat.jpg')
-#     table = read_csv_table()
-#     return render_template('index2.html', table=table, img_url=img_url)
-
-
-# @app.route('/img_file/<filename>')
-# def img_file(filename):
-#     return send_from_directory(img_dir, filename)
-
-
-# def read_csv_table():
-#     with open(filename, newline='') as f:
-#         reader = csv.reader(f)
-#         next(reader)
-#         table = [{'name': row[0], 'room': row[1], 'pic': row[2],'teln':row[3],'descript':row[4]} for row in reader]
-#     return table
-
-
-#================================================================================================================================
-
-
-# @app.route('/')
-# def index():
-#    print('Request for index page received')
-#    return render_template('index.html')
-
-# @app.route('/favicon.ico')
-# def favicon():
-#     return send_from_directory(os.path.join(app.root_path, 'static'),
-#                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-# @app.route('/hello', methods=['POST'])
-# def hello():
-#    name = request.form.get('name')
-
-#    if name:
-#        print('Request for hello page received with name=%s' % name)
-#        return render_template('hello.html', name = name)
-#    else:
-#        print('Request for hello page received with no name or blank name -- redirecting')
-#        return redirect(url_for('index'))
-
+@app.route('/delete_user', methods=['POST'])
+def delete_user():
+    global df
+    name = request.form['Name']
+    find=df[df['Name'] == name]
+    if find.shape[0]>0:
+        df.drop(find.index)
+        return 'Delete User Success'
+    return 'Delete User Failed'
 
 
 if __name__ == '__main__':
